a599cfa53d185dda25d019395fa9f05f3d3a4fde
Nicola Gallazzi <nicola.gallazzi.dev@gmail.com>
2024-09-19 18:18:41 +0200

    feat: Provide larger logo

--- src/pages/home-page.ts
+++ src/pages/home-page.ts
@@ -47,7 +47,7 @@ export class HomePage extends ReduxMixin(PolymerElement) {
 
         .hero-logo {
           --lazy-image-width: 100%;
-          --lazy-image-height: 76px;
+          --lazy-image-height: 150px;
           width: var(--lazy-image-width);
           height: var(--lazy-image-height);
           max-width: 240px;
