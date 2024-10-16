ae54e97b09ac1591dc986d79ec9d662a5f8100d6
Daniele Bonaldo <dany.bony@gmail.com>
2024-10-16 19:50:14 +0200

    Revert "Remove speakers from home page"
    This reverts commit 10a9918a48f3dafa25a57d0874259f4b0070db25.

--- src/pages/home-page.ts
+++ src/pages/home-page.ts
@@ -10,6 +10,7 @@ import '../elements/about-organizer-block';
 import '../elements/fork-me-block';
 import '../elements/map-block';
 import '../elements/partners-block';
+import '../elements/speakers-block';
 import '../elements/tickets-block';
 import { firebaseApp } from '../firebase';
 import { store } from '../store';
@@ -241,6 +242,7 @@ export class HomePage extends ReduxMixin(PolymerElement) {
         <fork-me-block></fork-me-block>
       </template>
       <about-block></about-block>
+      <speakers-block></speakers-block>
       <tickets-block id="tickets-block"></tickets-block>
       <about-organizer-block></about-organizer-block>
       <map-block></map-block>
